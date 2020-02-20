3a4b2c475356d9813608decf681ff021512c79f6
xuanngo10 <xuan.ngo10@gmail.com>
2020-02-20 07:29:28 -0800

    progress

--- js/script.js
+++ js/script.js
@@ -1,4 +1,33 @@
-// Hide Other Job Role Field initially
+////////////////////////////////////////////////////////////////
+// Variables
+////////////////////////////////////////////////////////////////
 const otherTitle = document.getElementById("other-title");
+const selectATheme = document.querySelector("option[value='select a theme']");
+const colorMenu = document.getElementById("color");
+const selectTShirtTheme = document.querySelector(
+  "option[value='please select a t-shirt theme']"
+);
 
+// Hide Other Job Role Field initially
 otherTitle.style.display = "none";
+
+////////////////////////////////////////////////////////////////
+// T-Shirt Info Section
+////////////////////////////////////////////////////////////////
+
+// hide select theme option
+selectATheme.hidden = "true";
+
+// hide color options initially
+for (let i = 0; i < colorMenu.length; i++) {
+  colorMenu[i].hidden = "true";
+}
+
+// append select a t-shirt theme option to html
+const selectColorHtml = `
+  <option value="please select a t-shirt theme">Please select a T-shirt theme</option>
+`;
+colorMenu.innerHTML += selectColorHtml;
+
+// select the "please select a t-shirt theme" option
+colorMenu[6].selected = "true";
